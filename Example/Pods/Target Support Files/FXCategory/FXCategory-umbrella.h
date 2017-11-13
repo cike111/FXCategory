94ce94dac624d0127e147c7d44430a3c879aa867
cike111 <1134916885@qq.com>
2017-11-13 15:30:28 +0800

    1

--- Example/Pods/Target Support Files/FXCategory/FXCategory-umbrella.h
+++ Example/Pods/Target Support Files/FXCategory/FXCategory-umbrella.h
@@ -10,9 +10,17 @@
 #endif
 #endif
 
+#import "NSString+PinYin4Cocoa.h"
+#import "NSObject+JSCategory.h"
+#import "UIView+Toast.h"
+#import "UIAlertView+AutoDismiss.h"
+#import "UIButton+FX.h"
+#import "UIButton+MRButton.h"
 #import "Color.h"
 #import "UIColor+Expanded.h"
 #import "UIColor+YUColor.h"
+#import "UITextField+FXDisabled.h"
+#import "UITextField+Helper.h"
 #import "UIView+Additions.h"
 #import "UIView+LayoutMethods.h"
 #import "UIView+SDExtension.h"
